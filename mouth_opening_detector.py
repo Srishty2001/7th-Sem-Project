f2924f2df398f201f142dcb385d4fc4f6dada013
Srishty Nanda <90861016+Srishty2001@users.noreply.github.com>
2023-10-16 13:08:35 +0530

    Mouth_opening_detector.py

--- mouth_opening_detector.py
+++ mouth_opening_detector.py
@@ -6,19 +6,23 @@ Created on Fri Jul 31 01:04:44 2020
 """
 
 import cv2
+import datetime
+import csv
+import tensorflow as tf
+from tensorflow.keras.regularizers import l2
 from face_detector import get_face_detector, find_faces
 from face_landmarks import get_landmark_model, detect_marks, draw_marks
 
 face_model = get_face_detector()
 landmark_model = get_landmark_model()
 outer_points = [[49, 59], [50, 58], [51, 57], [52, 56], [53, 55]]
-d_outer = [0]*5
+d_outer = [0] * 5
 inner_points = [[61, 67], [62, 66], [63, 65]]
-d_inner = [0]*3
-font = cv2.FONT_HERSHEY_SIMPLEX 
+d_inner = [0] * 3
+font = cv2.FONT_HERSHEY_SIMPLEX
 cap = cv2.VideoCapture(0)
 
-while(True):
+while (True):
     ret, img = cap.read()
     rects = find_faces(img, face_model)
     for rect in rects:
@@ -37,29 +41,43 @@ while(True):
 cv2.destroyAllWindows()
 d_outer[:] = [x / 100 for x in d_outer]
 d_inner[:] = [x / 100 for x in d_inner]
+mouth_counter = 0
 
-while(True):
-    ret, img = cap.read()
+while (True):
+    ret, image = cap.read()
     rects = find_faces(img, face_model)
+
+    image_saved = image
     for rect in rects:
-        shape = detect_marks(img, landmark_model, rect)
+        shape = detect_marks(image, landmark_model, rect)
         cnt_outer = 0
         cnt_inner = 0
-        draw_marks(img, shape[48:])
+        draw_marks(image, shape[48:])
         for i, (p1, p2) in enumerate(outer_points):
             if d_outer[i] + 1.5 < shape[p2][1] - shape[p1][1]:
-                cnt_outer += 1 
+                cnt_outer += 1
         for i, (p1, p2) in enumerate(inner_points):
-            if d_inner[i] + 0.5 <  shape[p2][1] - shape[p1][1]:
+            if d_inner[i] + 0.5 < shape[p2][1] - shape[p1][1]:
                 cnt_inner += 1
         if cnt_outer > 2 and cnt_inner > 2:
-            print('Mouth open')
-            cv2.putText(img, 'Mouth open', (30, 30), font,
-                    1, (0, 255, 255), 2)
+            cv2.putText(image, 'Mouth open', (30, 30), font,
+                        1, (0, 255, 255), 2)
+            mouth_counter += 1
+            if mouth_counter == 70:
+                print('Mouth open')
+                tf.keras.utils.save_img("./models/image/image3.png", image_saved)
+                head = [
+                    ['Talking', './models/image/image3.png', datetime.datetime.now()]
+                ]
+                file = open('models/data.csv', 'a', newline='')
+                writer = csv.writer(file)
+                writer.writerow(head)
+                file.close()
+                mouth_counter = 0
         # show the output image with the face detections + facial landmarks
-    cv2.imshow("Output", img)
+    cv2.imshow("Output", image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
